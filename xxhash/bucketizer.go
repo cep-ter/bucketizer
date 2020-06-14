739b382adcf20a529fcd225f954a07d5c96fe9e7
ehsan <ehsanroman74@gmail.com>
2020-06-14 16:13:03 +0430

    add seed feature to have different buckets with every run, implement bucket method for other types (including int, int8, int16, int32, int64, float32, float64), refactor some code, remove some duplicate lines

--- xxhash/bucketizer.go
+++ xxhash/bucketizer.go
@@ -1,6 +1,7 @@
 package xxhash
 
 import (
+	"fmt"
 	"github.com/cespare/xxhash"
 	"github.com/ehsangolshani/bucketizer"
 	"github.com/pkg/errors"
@@ -10,22 +11,11 @@ type XXHASHBucketizer struct {
 	Buckets      []bucketizer.Bucket
 	weightSum    uint64
 	bucketRanges []uint64
-}
-
-func (b XXHASHBucketizer) BucketString(value string) (int, error) {
-	hashDigest := xxhash.Sum64String(value)
-	reminder := hashDigest % b.weightSum
-	n := len(b.bucketRanges)
-	for i := 0; i < n-1; i++ {
-		if reminder >= b.bucketRanges[i] && reminder < b.bucketRanges[i+1] {
-			return i, nil
-		}
-	}
-	return 0, errors.New("invalid reminder value")
+	seed         []byte
 }
 
 func (b XXHASHBucketizer) BucketBytes(value []byte) (int, error) {
-	hashDigest := xxhash.Sum64(value)
+	hashDigest := xxhash.Sum64(append(value, b.seed...))
 	reminder := hashDigest % b.weightSum
 	n := len(b.bucketRanges)
 	for i := 0; i < n-1; i++ {
@@ -36,47 +26,47 @@ func (b XXHASHBucketizer) BucketBytes(value []byte) (int, error) {
 	return 0, errors.New("invalid reminder value")
 }
 
-func (b XXHASHBucketizer) BucketInt(value int) (int, error) {
+func (b XXHASHBucketizer) BucketString(value string) (int, error) {
+	valueInBytes := []byte(value)
+	return b.BucketBytes(valueInBytes)
+}
 
-	return 0, nil
+func (b XXHASHBucketizer) BucketInt(value int) (int, error) {
+	valueInBytes := []byte(fmt.Sprintf("%d", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketInt8(value int8) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%d", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketInt16(value int16) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%d", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketInt32(value int32) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%d", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketInt64(value int64) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%d", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketFloat64(value float64) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%g", value))
+	return b.BucketBytes(valueInBytes)
 }
 
 func (b XXHASHBucketizer) BucketFloat32(value float32) (int, error) {
-
-	return 0, nil
+	valueInBytes := []byte(fmt.Sprintf("%g", value))
+	return b.BucketBytes(valueInBytes)
 }
 
-func (b XXHASHBucketizer) BucketInterface(value interface{}) (int, error) {
-
-	return 0, nil
-}
-
-func NewXXHASHBucketizer(buckets ...bucketizer.Bucket) XXHASHBucketizer {
+func NewXXHASHBucketizer(seed int64, buckets ...bucketizer.Bucket) XXHASHBucketizer {
 	var sumOfWeights uint64 = 0
 	var bucketRanges []uint64
 	bucketRanges = append(bucketRanges, 0)
@@ -89,5 +79,6 @@ func NewXXHASHBucketizer(buckets ...bucketizer.Bucket) XXHASHBucketizer {
 		Buckets:      buckets,
 		weightSum:    sumOfWeights,
 		bucketRanges: bucketRanges,
+		seed:         []byte(fmt.Sprintf("%d", seed)),
 	}
 }
